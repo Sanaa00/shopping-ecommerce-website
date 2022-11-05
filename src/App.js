af0aa5073a3fc24b5c9f10fb752cbbce15a20113
Sana Rzgar <sanarizgar34@gmail.com>
2022-11-05 15:24:19 +0300

    creating Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import Shop from "./Pages/Shop";
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        spi: "#eaeaec",
+        rasasy: "#a3a4a5",
+        mory_kal: "#a3a3ac",
+        rash: "#3d3d3d",
+        xanay: "#92553d",
+      },
+      fontFamily: {},
+    },
+  },
+  plugins: [],
+};
