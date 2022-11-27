c6c4403ed4c998f1c70f884abb20de21c0a3340a
Sana Rzgar <sanarizgar34@gmail.com>
2022-11-27 19:46:37 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import axios from "axios";
